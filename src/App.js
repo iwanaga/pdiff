9ff91cccd16c8a18d45ab6eaba42220c87686592
Yoshihiro <iwanaga.blackie@gmail.com>
2022-03-21 23:33:38 +0900

    input form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import { GoDiff } from 'react-icons/go';
-import { AppBar, Autocomplete, createFilterOptions, Stack, TextField, Toolbar, Typography } from '@mui/material';
+import { ImFilesEmpty } from 'react-icons/im';
+import { GoGitPullRequest } from 'react-icons/go'
+import { Autocomplete, Button, createFilterOptions, Stack, TextField, Toolbar, Typography } from '@mui/material';
+import { FiDownloadCloud } from 'react-icons/fi';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -30,20 +32,23 @@ function App() {
     stringify: option => option.accountName
   });
 
+  // Input fields
+  const [propertyHostname, setPropertyHostname] = useState('');
+  const [versionBefore, setVersionBefore] = useState('');
+  const [versionAfter,  setVersionAfter ] = useState('');
+
+  // diff
+  const [before, setBefore] = useState('');
+  const [after,  setAfter ] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1><GoDiff />&nbsp;&nbsp; pdiff</h1>
+        <h1>&nbsp;<ImFilesEmpty />&nbsp; pdiff</h1>
       </header>
 
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h6">Input</Typography>  
-        </Toolbar>
-      </AppBar>
-
       <div className="form-box">
-        <Stack spacing={1} sx={{width: 400}}>
+        <Stack spacing={2} sx={{width: 400}}>
           <Autocomplete
             options={options}
             getOptionLabel={(option) => typeof option === 'string' ? option : option.accountName }
@@ -53,7 +58,6 @@ function App() {
             value={accountName}
             label="Account Name"
             required
-            className="wide-text-field"
             renderInput={(params) => <TextField {...params} label="Account Name" variant="standard" />}
             isOptionEqualToValue={(option, value) => option.accountName }
             onChange={(event, newValue) => {
@@ -71,11 +75,48 @@ function App() {
             onChange={(event) => { if (event.target) { setAccountSwitchKey(event.target.value) } }}
             label="Account Switch Key"
             required
-            className="wide-text-field"
             disabled
           />
+
+          <TextField
+            variant="standard"
+            value={propertyHostname}
+            onChange={(event) => { if (event.target) { setPropertyHostname(event.target.value) } }}
+            label="Property Hostname"
+            required
+          />
+
+          <Stack direction="row" spacing={2}>
+            <TextField
+              variant="standard"
+              value={versionBefore}
+              onChange={(event) => { if (event.target) { setVersionBefore(event.target.value) } }}
+              label="Version From"
+              required
+              className="narrow-text-field"
+            />
+
+            <TextField
+              variant="standard"
+              value={versionAfter}
+              onChange={(event) => { if (event.target) { setVersionAfter(event.target.value) } }}
+              label="Version To"
+              required
+              className="narrow-text-field"
+            />
+          </Stack>
         </Stack>
+
+        <div className="button-wrap">
+          <Button variant="contained" startIcon={<FiDownloadCloud />}>Show diff</Button>
+        </div>
       </div>
+
+      {before && after ?
+      <Toolbar>
+        <Typography variant="h5" sx={{ flexGrow: 1 }}><GoGitPullRequest /> &nbsp;diff</Typography>
+      </Toolbar>
+      : <></>}
     </div>
   );
 }
