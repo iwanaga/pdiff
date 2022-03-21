924fbd248b219860cb3dc07b2a726f78d465033b
Yoshihiro <iwanaga.blackie@gmail.com>
2022-03-21 17:03:09 +0900

    API lib to get accountSwitchKey

---
+++ server/api/lib/IdentityManagement.mjs
@@ -0,0 +1,25 @@
+import EdgeGrid from 'edgegrid';
+import os from 'os';
+
+const edgeGrid = new EdgeGrid({
+  path: `${os.homedir()}/.edgerc`,
+  section: 'test_center'
+});
+
+export const listAccountSwitchKeys = async (accountName) => {
+  edgeGrid.auth({
+    path: `/identity-management/v2/api-clients/self/account-switch-keys?search=${accountName}`,
+    method: 'GET',
+    header: {},
+  });
+
+  return new Promise((resolve, reject) => {
+    edgeGrid.send((err, response, bodyString) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve(JSON.parse(bodyString));
+    });
+  });
+};
